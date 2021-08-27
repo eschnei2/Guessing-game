b181b46e2eea07d5953f3824967f2d072aec4fb6
eschnei2 <everettschneider@gmail.com>
2021-08-27 09:29:03 -0500

    first step done

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a secret number between 1 and 100");
+
+            string userAnswer = Console.ReadLine();
+            Console.WriteLine(userAnswer);
+
+
+        }
+    }
+}
