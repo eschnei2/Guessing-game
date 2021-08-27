b2b99449a60448cbaa6f44c4d997f8fcde0e3136
eschnei2 <everettschneider@gmail.com>
2021-08-27 09:42:24 -0500

    phase 2 completed

--- Program.cs
+++ Program.cs
@@ -9,7 +9,16 @@ namespace GuessingGame
             Console.WriteLine("Guess a secret number between 1 and 100");
 
             string userAnswer = Console.ReadLine();
-            Console.WriteLine(userAnswer);
+            int parseAnswer = int.Parse(userAnswer);
+            int randomNum = 42;
+
+            if (parseAnswer == randomNum)
+            {
+                Console.WriteLine("You got it right!");
+            }
+            else {
+                Console.WriteLine("WRONG WRONG WRONG!");
+            }
 
 
         }
